8cfdede1c44ca78d9848db09fb23e23f71602341
KoreanTuna <songsee0528@gmail.com>
2025-06-27 21:52:26 +0900

    🎉 프로젝트 시작

---
+++ README.md
@@ -0,0 +1,4 @@
+# bloc_deep_dive
+
+A new Flutter project.
+# Bloc-Deep-Dive
---
+++ android/app/src/main/kotlin/com/example/bloc_deep_dive/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.bloc_deep_dive
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
