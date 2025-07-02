182af61c31ed63d117814a34a013d0882cf13d60
KoreanTuna <songsee0528@gmail.com>
2025-07-02 21:18:01 +0900

    MultiBloc 리스너로 마이그레이션 진행

--- lib/presentation/login/widget/login_form.dart
+++ lib/presentation/login/widget/login_form.dart
@@ -20,31 +20,35 @@ class LoginForm extends StatelessWidget {
     final bool isInProgressOrSuccess = context.select(
       (LoginBloc bloc) => bloc.state.status.isInProgressOrSuccess,
     );
-
-    return BlocListener<LoginBloc, LoginState>(
-      listener: (context, state) {
-        if (state.status.isFailure) {
-          ScaffoldMessenger.of(context)
-            ..hideCurrentSnackBar()
-            ..showSnackBar(
-              SnackBar(
-                content: Text(
-                  '로그인에 실패했습니다.',
-                  style: TextStyle().body2.copyWith(color: ColorStyle.white),
-                ),
-              ),
-            );
-        }
-
-        if (state.status.isSuccess) {
-          context.read<UserBloc>().add(
-            UserDataRequested(userId: state.user.id),
-          );
-        }
-      },
-      child: BlocListener<UserBloc, UserState>(
-        listener: (context, state) {
-          if (state is UserError) {
+    return MultiBlocListener(
+      listeners: [
+        BlocListener<LoginBloc, LoginState>(
+          listenWhen:
+              (previous, current) =>
+                  previous.status != current.status &&
+                  current.status.isInProgressOrSuccess,
+          listener: (context, state) {
+            if (state.status.isInProgress) {
+              ScaffoldMessenger.of(context)
+                ..hideCurrentSnackBar()
+                ..showSnackBar(
+                  SnackBar(
+                    content: Text(
+                      '로그인 중입니다...',
+                      style: TextStyle().body2.copyWith(
+                        color: ColorStyle.white,
+                      ),
+                    ),
+                  ),
+                );
+            }
+          },
+        ),
+        BlocListener<UserBloc, UserState>(
+          listenWhen:
+              (previous, current) =>
+                  previous is! UserError && current is UserError,
+          listener: (context, state) {
             ScaffoldMessenger.of(context)
               ..hideCurrentSnackBar()
               ..showSnackBar(
@@ -55,90 +59,95 @@ class LoginForm extends StatelessWidget {
                   ),
                 ),
               );
-            return;
-          }
+          },
+        ),
+        BlocListener<UserBloc, UserState>(
+          listenWhen:
+              (previous, current) =>
+                  previous is! UserLoaded && current is UserLoaded,
+          listener: (context, state) {
+            /// 사용자 정보 불러오기 성공
+            context.goNamed(RouterPath.splash);
+          },
+        ),
+      ],
+      child: Stack(
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: SizedBox(
+              width: context.screenSize.width,
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  SizedBox.shrink(),
+                  Column(
+                    spacing: 8,
+                    children: [
+                      SvgImageWidget(
+                        width: 78,
+                        height: 78,
+                        fit: BoxFit.cover,
+                        imagePath: SvgImagePath.logo,
+                      ),
+                      Text(
+                        'DOOR STAMP',
+                        style: TextStyle().subTitle3,
+                      ),
+                    ],
+                  ),
 
-          /// 사용자 정보 불러오기 성공
-          context.goNamed(RouterPath.splash);
-        },
-        child: Stack(
-          children: [
-            Padding(
-              padding: const EdgeInsets.all(16.0),
-              child: SizedBox(
-                width: context.screenSize.width,
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    SizedBox.shrink(),
-                    Column(
-                      spacing: 8,
-                      children: [
-                        SvgImageWidget(
-                          width: 78,
-                          height: 78,
-                          fit: BoxFit.cover,
-                          imagePath: SvgImagePath.logo,
-                        ),
-                        Text(
-                          'DOOR STAMP',
-                          style: TextStyle().subTitle3,
-                        ),
-                      ],
-                    ),
-
-                    Column(
-                      spacing: 12,
-                      children: [
-                        Text(
-                          ' 간편하게 로그인하고\n다양한 서비스를 이용하세요',
-                          style: TextStyle().body2,
-                          textAlign: TextAlign.center,
-                        ),
-                        Column(
-                          children:
-                              SSOType.values
-                                  .map(
-                                    (ssoType) => Padding(
-                                      padding: const EdgeInsets.only(
-                                        bottom: 12.0,
-                                      ),
-                                      child: _LoginButton(
-                                        ssoType: ssoType,
-                                        onLoginButtonPressed: () {
-                                          context.read<LoginBloc>().add(
-                                            LoginSubmitted(ssoType),
-                                          );
-                                        },
-                                      ),
+                  Column(
+                    spacing: 12,
+                    children: [
+                      Text(
+                        ' 간편하게 로그인하고\n다양한 서비스를 이용하세요',
+                        style: TextStyle().body2,
+                        textAlign: TextAlign.center,
+                      ),
+                      Column(
+                        children:
+                            SSOType.values
+                                .map(
+                                  (ssoType) => Padding(
+                                    padding: const EdgeInsets.only(
+                                      bottom: 12.0,
                                     ),
-                                  )
-                                  .toList(),
+                                    child: _LoginButton(
+                                      ssoType: ssoType,
+                                      onLoginButtonPressed: () {
+                                        context.read<LoginBloc>().add(
+                                          LoginSubmitted(ssoType),
+                                        );
+                                      },
+                                    ),
+                                  ),
+                                )
+                                .toList(),
+                      ),
+                      Text(
+                        '로그인 시 이용약관 / 개인정보 처리방침 동의로 간주합니다',
+                        style: TextStyle().detail.copyWith(
+                          color: ColorStyle.coolGray500,
                         ),
-                        Text(
-                          '로그인 시 이용약관 / 개인정보 처리방침 동의로 간주합니다',
-                          style: TextStyle().detail.copyWith(
-                            color: ColorStyle.coolGray500,
-                          ),
-                        ),
-                      ],
-                    ),
-                  ],
-                ),
+                      ),
+                    ],
+                  ),
+                ],
               ),
             ),
-            if (isInProgressOrSuccess)
-              Container(
-                width: context.screenSize.width,
-                height: context.screenSize.height,
-                color: ColorStyle.gray850.withValues(alpha: 0.3),
-                child: const Center(
-                  child: CupertinoActivityIndicator(),
-                ),
+          ),
+          if (isInProgressOrSuccess)
+            Container(
+              width: context.screenSize.width,
+              height: context.screenSize.height,
+              color: ColorStyle.gray850.withValues(alpha: 0.3),
+              child: const Center(
+                child: CupertinoActivityIndicator(),
               ),
-          ],
-        ),
+            ),
+        ],
       ),
     );
   }
